ecfb3c16d0a4f5219fe7862cf63b8cc4a855eb66
chvndler <chan@ady.world>
2022-02-15 23:03:40 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,63 +12,59 @@ import { Spacer } from '@/ui/Spacer';
 
 import { TwitterLogoIcon } from '@radix-ui/react-icons';
 
-import { styled } from 'stitches.config';
-
-const CenterBox = styled('div', {
-  minHeight: '100vh',
-  padding: '0 0.5rem',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  // alignItems: 'center',
-  height: '100vh',
-});
-
 // APP BEGIN / HOMEPAGE
 const Index = () => {
   return (
-    <Box css={{ minWidth: '100vw' }}>
+    <Box css={{ minWidth: '100vw', height: '100%', overflow: 'visible' }}>
       <Navbar />
-      <CenterBox>
-        <Container size="3">
-          <Text size="3" css={{ paddingBottom: '50px', lineHeight: '1.4' }}>
-            My name is Chandler, most call me Chan. I'm currently based in Charlotte, NC. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
+      <Container size="2" css={{ paddingTop: '200px' }}>
+        <Heading
+          size="4"
+          css={{
+            width: '100%',
+            alignItems: 'left',
+            textAlign: 'left',
+            paddingBottom: '10px',
+            lineHeight: '1.4',
+          }}
+        >
+          <span>Hi, I'm Chan.</span>
+        </Heading>
 
-          <Heading
-            size="2"
-            css={{
-              width: '100%',
-              alignItems: 'left',
-              textAlign: 'left',
-              paddingBottom: '50px',
-              lineHeight: '1.4',
-            }}
-          >
-            <span>Connect</span>
-          </Heading>
+        <Text size="3" css={{ paddingBottom: '50px', lineHeight: '1.4' }}>
+          My name is Chandler, most call me Chan. I'm currently based in Charlotte, NC. Lorem ipsum dolor sit amet,
+          consectetur adipiscing elit.
+        </Text>
 
-          <TwitterLogoIcon />
+        <Heading
+          size="2"
+          css={{
+            width: '100%',
+            alignItems: 'left',
+            textAlign: 'left',
+            paddingBottom: '50px',
+            lineHeight: '1.4',
+          }}
+        >
+          <span>Connect</span>
+        </Heading>
 
-          <Spacer />
-          <Heading
-            size="2"
-            css={{
-              width: '100%',
-              alignItems: 'left',
-              textAlign: 'left',
-              paddingBottom: '50px',
-              lineHeight: '1.4',
-            }}
-          >
-            <span>Current Projects</span>
-          </Heading>
-        </Container>
-      </CenterBox>
+        <TwitterLogoIcon />
+
+        <Spacer />
+        <Heading
+          size="2"
+          css={{
+            width: '100%',
+            alignItems: 'left',
+            textAlign: 'left',
+            paddingBottom: '50px',
+            lineHeight: '1.4',
+          }}
+        >
+          <span>Current Projects</span>
+        </Heading>
+      </Container>
     </Box>
   );
 };
