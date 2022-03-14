7f9f8e13eaaed70680ce5e8589866b4979d9b69e
chvndler <chan@ady.world>
2022-03-14 09:20:44 -0400

    Update _chvn.js

--- pages/_chvn.js
+++ pages/_chvn.js
@@ -1,19 +1,18 @@
-import Page from '@components/page'
+import Page from '@components/page';
 
 const About = () => {
   return (
-    <Page description="Hi, I'm Chan. Frontend developer and designer, mechanical keyboard enthusiast, practicing minimalist, and trance lover in search of flow.">
+    <Page description="Hi, I'm Chan. Frontend developer and designer.">
       <article>
         <h1>Chandler Chappell</h1>
 
         <p>
-          Frontend developer and designer. I make beautiful, minimal websites
-          using modern technologies. I specialize in design systems and
+          Frontend developer and designer. I make beautiful, minimal websites using modern technologies. I specialize in design systems and
           component based UI libraries.
         </p>
       </article>
     </Page>
-  )
-}
+  );
+};
 
-export default About
+export default About;
