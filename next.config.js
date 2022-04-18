13a7b57b98bc51d28e18e3a42456e48584d05835
chvndler <chan@ady.world>
2022-04-18 01:57:44 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,18 +12,4 @@ module.exports = withContentlayer({
   experimental: {
     urlImports: ['https://cdn.ady.systems/', 'https://cdn.skypack.dev/'],
   },
-
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-
-    return config;
-  },
 });
