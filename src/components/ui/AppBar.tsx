57e132b8a3337b5d093ad02baf10dbac961c68db
chvndler <70621588+chvndler@users.noreply.github.com>
2022-05-03 10:11:18 -0400

    Update AppBar.tsx

--- src/components/ui/AppBar.tsx
+++ src/components/ui/AppBar.tsx
@@ -10,7 +10,7 @@ const GradientBar = styled('div', {
   left: '0',
   top: '0',
   // background: 'linear-gradient(110deg, $blue9 0%, $orange10 40%, $lime9 100%)',
-  background: 'linear-gradient(110deg, #3A1C71 0%, #D76D77 40%, #FFAF7B 100%)',
+  background: 'linear-gradient(110deg, #ffd89b 0%, #D76D77 40%, #19547b 100%)',
   // background: '-webkit-linear-gradient(to right, #5D26C1, #a17fe0, #59C173)',
 
   //background: '#59C173',
